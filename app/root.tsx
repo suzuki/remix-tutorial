9694a19648906bfdcdda7761a37c724bb067402c
Norio Suzuki <norio.suzuki@gmail.com>
2024-01-16 08:25:48 +0900

    Add edit contact page

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { json } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
 import type { LinksFunction } from '@remix-run/node';
 import {
   Form,
@@ -12,7 +12,7 @@ import {
   useLoaderData,
 } from "@remix-run/react";
 import appStylesHref from './app.css';
-import { getContacts } from './data';
+import { createEmptyContact, getContacts } from './data';
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
@@ -21,6 +21,11 @@ export const links: LinksFunction = () => [
 export const loader = async () => {
   const contacts = await getContacts();
   return json({ contacts });
+};
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
 };
 
 
---
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -0,0 +1,85 @@
+import { json, type ActionFunctionArgs, type LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { getContact, updateContact } from "~/data";
+
+export const loader = async ({
+  params,
+}: LoaderFunctionArgs) => {
+  invariant(params.contactId, 'Missing contactId param');
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response('Not Found', { status: 404 });
+  }
+  return json({ contact });
+};
+
+
+export const action = async ({
+  params,
+  request,
+}: ActionFunctionArgs) => {
+  invariant(params.contactId, 'Missing contactid param');
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData);
+  await updateContact(params.contactId, updates);
+  return redirect(`/contacts/${params.contactId}`);
+};
+
+
+export default function EditContact() {
+  const { contact } = useLoaderData<typeof loader>();
+
+  return (
+    <Form id="contact-form" method="post">
+      <p>
+        <span>Name</span>
+        <input
+          defaultValue={contact.first}
+          aria-label="First name"
+          name="first"
+          type="text"
+          placeholder="First"
+        />
+        <input
+          defaultValue={contact.last}
+          aria-label="Last name"
+          name="last"
+          type="text"
+          placeholder="Last"
+        />
+      </p>
+      <label>
+        <span>Twitter</span>
+        <input
+          defaultValue={contact.twitter}
+          name="twitter"
+          type="text"
+          placeholder="@jack"
+        />
+      </label>
+      <label>
+        <span>Avatar URL</span>
+        <input
+          defaultValue={contact.avatar}
+          aria-label="Avatar URL"
+          name="avatar"
+          type="text"
+          placeholder="https://example.com/avatar.jpg"
+        />
+      </label>
+      <label>
+        <span>Noptes</span>
+        <textarea
+          defaultValue={contact.notes}
+          name="notes"
+          rows={6}
+        />
+      </label>
+      <p>
+        <button type="submit">Save</button>
+        <button type="button">Cancel</button>
+      </p>
+    </Form>
+  );
+};
